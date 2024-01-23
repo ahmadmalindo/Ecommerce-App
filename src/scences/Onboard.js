02422dd7dbb13a7c3ee4fedc2fc576494a3cb7a5
Malindo <indoelek6@gmail.com>
2024-01-23 09:16:23 +0700

    adjustment in tablet ios

--- src/scences/Onboard.js
+++ src/scences/Onboard.js
@@ -1,6 +1,6 @@
 import { Button, Container, Gap } from "components/global"
 import React from "react"
-import { Image, StyleSheet, Text, View } from "react-native"
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
 import MMKVStorage from "react-native-mmkv-storage"
 import normalize from "react-native-normalize"
 import { fonts, stylesFonts } from "utils/fonts"
@@ -15,7 +15,7 @@ function Onboard({ navigation }) {
     const isLogin = storage.getBool("isLogin")
 
     return (
-        <View>
+        <ScrollView>
             <Image source={require ('assets/images/ic_onboard.png')} resizeMethod="scale" resizeMode="contain" style={styles.img}/>
             <View style={{padding: normalize(16)}}>
                 <Text style={[stylesFonts.Heading_2, {fontSize: normalize(22), fontFamily: fonts.bold, lineHeight: normalize(40)}]}>Kebutuhan untuk menunjang kecantikanmu, semua tersedia{'\n'}di MySalon 💅🏻</Text>
@@ -33,7 +33,7 @@ function Onboard({ navigation }) {
                     }}
                 />
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
